9cf48506b07bcbd4c9bdc96939fd9ea3164b6257
Edgar <davidnavarro81230@outlook.com>
2023-08-09 07:32:13 -0500

    Creating  categories table

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Category> Categories {get; set;}
+	}
+}
+
